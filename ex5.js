cff4915e735602d8cef5f7a6e39e1dce55cfd183
MMarek <mildnermarek@gmail.com>
2020-02-08 20:36:49 +0100

    set interval

--- ex5.js
+++ ex5.js
@@ -1,31 +1,13 @@
-const sinus = function (x, iterNum) {
-    let power = 1;
-    let a;
-    let b;
-    let division;
-    const array = [];
+function hello(stop) {
+    let number = 0;
+    const id = setInterval(function () {
+        number++;
+        console.log('Witam... i tak pięć razy, co jedną sekundę :)', number);
 
-    let sin = 0;
-
-    for (let i = 1; i <= iterNum ; i++) {
-        a = Math.pow(x, power);
-        b = 1;
-        for (let j = 1; j <= power; j++) {
-            b= b * j;
+        if (number >= stop) {
+            clearInterval(id)
         }
-        power += 2;
+    }, 1000)
+}
 
-        division = a/b;
-        array.push(division);
-    }
-    for (let i = 0; i < iterNum; i++) {
-        if(i%2 === 0){
-            sin += array[i];
-        }else{
-            sin -= array[i];
-        }
-    }
-    return sin;
-};
-
-console.log(sinus(7, 10));
+hello(5);
